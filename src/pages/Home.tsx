7538f37bef0acca81ea1bfa4643bbeeb25e83f83
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-04 20:03:19 +0300

    Under development

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,13 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <Grid columns={1} gap="1rem">
-        <div className={styles["grid-item-purple"]}></div>
+        <div
+          className={styles["grid-item-purple"]}
+        ></div>
       </Grid>
       <h2>Creative & Development</h2>
-      <Grid columns={2} gap="1rem">
-        <div className={styles["grid-item-blank"]}></div>
-        <div className={styles["grid-item-blank"]}><p className={styles.lead}>That's what makes Digitaltableteur — a design-led digital studio. Based online, working globally.
+      <Grid columns={3} gap="1rem">
+        <div className={styles["grid-item-blank"]} style={{ gridColumn: "2 / span 2" }}><p className={styles.lead}>That's what makes Digitaltableteur — a design-led digital studio. Based online, working globally.
 Helping brands and individuals rethink, simplify, and stand out. Whether it’s strategy, identity, interface, or experience — DS cuts through the noise to deliver clarity, presence, and expression.
 Well known for obsession to craft, systems, and a little bit of chaos.</p>
 </div>
