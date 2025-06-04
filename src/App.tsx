afb1bbb801b134c30088d9998535535078211832
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-04 12:31:01 +0300

    Removes ProgressMessage and func

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import Home from "./pages/Home";
 import Work from "./pages/Work";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import ProgressMessage from "./components/ProgressMessage";
 
 function App() {
   return (
     <Router>
       <Layout>
-        {process.env.REACT_APP_SHOW_PROGRESS_MESSAGE === "true" && (
-          <ProgressMessage />
-        )}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
