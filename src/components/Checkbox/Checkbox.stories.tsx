f5aadfc33fe8fa53f50f6388e4fe4584f60223a0
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-06 07:26:28 +0300

    Checkbox refactoring

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -31,26 +31,3 @@ Indeterminate.args = {
   checked: false,
   indeterminate: true,
 };
-
-export const TriState = () => {
-  const [checked, setChecked] = React.useState(false);
-  const [indeterminate, setIndeterminate] = React.useState(true);
-
-  const handleChange = (nextChecked: boolean) => {
-    if (indeterminate) {
-      setChecked(true);
-      setIndeterminate(false);
-    } else {
-      setChecked(nextChecked);
-    }
-  };
-
-  return (
-    <Checkbox
-      label="Tri-state Checkbox"
-      checked={checked}
-      indeterminate={indeterminate}
-      onChange={handleChange}
-    />
-  );
-};
