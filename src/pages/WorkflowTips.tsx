14f6a93b72daf35c72683e271405b960d5e78360
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-07 09:02:07 +0300

    Add blog section with article pages

---
+++ src/pages/WorkflowTips.tsx
@@ -0,0 +1,13 @@
+import styles from "./Article.module.css";
+
+const WorkflowTips = () => (
+  <article className={styles.article}>
+    <header>
+      <h1>Workflow Tips</h1>
+      <p className={styles.author}>By Digitaltableteur</p>
+    </header>
+    <p>We are preparing this article. Check back soon for insights into our day-to-day process.</p>
+  </article>
+);
+
+export default WorkflowTips;
