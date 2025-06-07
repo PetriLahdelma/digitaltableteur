4742ab7c4dffe37aea63188a14e4782c6e14ba84
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-07 09:02:07 +0300

    Add blog section with article pages

--- src/patterns/Header/Header.tsx
+++ src/patterns/Header/Header.tsx
@@ -12,6 +12,7 @@ const Header = () => {
           <li><a href="/">Home</a></li>
           <li><a href="/work">Work</a></li>
           <li><a href="/about">About</a></li>
+          <li><a href="/blog">Blog</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
