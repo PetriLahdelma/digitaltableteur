f9fa911e434e8b8a7fe05414f68581f73f29e4d6
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-10 10:36:50 +0300

    Cookie consent

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Link.module.css";
-import { FaExternalLinkAlt } from "react-icons/fa";
 
 interface LinkProps {
   href: string;
@@ -10,17 +9,12 @@ interface LinkProps {
 
 const Link: React.FC<LinkProps> = ({ href, size = "M", children }) => {
   const isExternal =
-    typeof href === "string" && !href.includes("digitaltableteur.com");
+    !href.startsWith("/") && !href.includes("digitaltableteur.com");
 
   return (
-    <a
-      href={href}
-      className={`${styles.link} ${styles[size]}`}
-      target={isExternal ? "_blank" : undefined}
-      rel={isExternal ? "noopener noreferrer" : undefined}
-    >
+    <a href={href} className={`${styles.link} ${styles[size]}`}>
       {children}
-      {isExternal && FaExternalLinkAlt({ className: styles.icon })}
+      {isExternal && <span className={styles.externalIcon}>🔗</span>}{" "}
     </a>
   );
 };
