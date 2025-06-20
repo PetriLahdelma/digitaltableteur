9650882bcad6a7995d87ca97e86c61b1d5ee4e47
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-20 10:14:30 +0300

    Quick fixxx

--- src/patterns/Header/Header.tsx
+++ src/patterns/Header/Header.tsx
@@ -4,13 +4,23 @@ import styles from "./Header.module.css";
 import "../../styles/variables.css";
 import "../../styles/fonts.css";
 import Logo from "../../assets/images/01jy60fd46fxwvk450w70bmyzm_1750401080.webp";
+import ScrollTitle from "../../components/ScrollTitle/ScrollTitle";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Link to="/">
-        <img src={Logo} alt="Digitaltableteur Logo" className={styles.logo} />
+        <img
+          src={Logo}
+          alt="Digitaltableteur Logo"
+          className={`${styles.logo} ${
+            typeof window !== "undefined" && window.scrollY > 50
+              ? styles.logoScrolled
+              : ""
+          }`}
+        />
       </Link>
+      {typeof window !== "undefined" && <ScrollTitle />}
 
       <nav>
         <ul className={styles.nav}>
