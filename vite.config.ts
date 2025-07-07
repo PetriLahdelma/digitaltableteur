21511733dfbcc4a1a46d107c33ad365e45e05e54
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-07-07 22:57:36 +0300

    Try 404 hack

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/404.html",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   server: {
     host: "0.0.0.0", // This allows access to all devices on your local network
     port: 5173,
