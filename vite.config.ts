fd84695a336577985dd4621c74af2559ebfe59e6
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-07-07 22:59:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/404.html",
-          dest: "",
-        },
-      ],
-    }),
-  ],
-  server: {
-    host: "0.0.0.0", // This allows access to all devices on your local network
-    port: 5173,
-  },
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import("vite-plugin-static-copy");
+  return {
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "public/404.html",
+            dest: "",
+          },
+        ],
+      }),
+    ],
+    server: {
+      host: "0.0.0.0", // This allows access to all devices on your local network
+      port: 5173,
+    },
+  };
 });
