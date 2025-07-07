f89ac0402db389b9a7d1812f47908e1d87ec4d00
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-07-07 22:04:31 +0300

    Comment out UnderDevelopment check in App
    The conditional rendering of the UnderDevelopment component has been commented out, allowing the main application to render regardless of the bypass check. This may be for development or testing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -46,9 +46,9 @@ function isBypass() {
 }
 
 function App() {
-  if (!isBypass()) {
-    return <UnderDevelopment />;
-  }
+  // if (!isBypass()) {
+  //   return <UnderDevelopment />;
+  // }
 
   return (
     <Router>
