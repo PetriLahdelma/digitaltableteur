0b4059f48876bae8157595b92e9030a50cc90588
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-07-09 00:55:50 +0300

    Update save-contact.js

--- api/save-contact.js
+++ api/save-contact.js
@@ -1,10 +1,17 @@
 import { MongoClient } from "mongodb";
 
 export default async function handler(request, response) {
-  response.setHeader(
-    "Access-Control-Allow-Origin",
+  const allowedOrigins = [
     "https://digitaltableteur.com",
-  );
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ];
+  const origin = request.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    response.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    response.setHeader("Access-Control-Allow-Origin", allowedOrigins[0]);
+  }
   response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   response.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (request.method === "OPTIONS") {
