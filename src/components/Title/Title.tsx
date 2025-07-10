bbd83f5ca74ecb0d479c625ce437b938dc5ab48d
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-07-10 21:22:20 +0300

    Revert "Add Next.js SSG"

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Title.module.css";
+import "../../styles/variables.css";
 
 type TitleSize = "S" | "M" | "L" | "XL";
 type TitleTerminals = "sans" | "serif";
