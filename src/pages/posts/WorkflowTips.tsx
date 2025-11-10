144673739fff36ac07c2628556dc24eb0108175c
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-11-10 08:06:25 +0200

    chat fix + prettier

--- src/pages/posts/WorkflowTips.tsx
+++ src/pages/posts/WorkflowTips.tsx
@@ -218,7 +218,7 @@ const WorkflowTips = () => {
         <hr
           style={{
             border: "none",
-            borderTop: "3px solid var(--color-primary)",
+            borderTop: "2px solid var(--color-primary)",
             margin: "3rem 0 1.5rem",
           }}
         />
