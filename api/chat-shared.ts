43dbdec1d16b2519466fbf5b852d382b7521a718
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-11-11 13:17:25 +0200

    Add localhost and IP-based entries to allowed-headers

--- api/chat-shared.ts
+++ api/chat-shared.ts
@@ -7,7 +7,9 @@ export const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5176",
   "http://localhost:3000",
-  "http://localhost:3001",
+  "http://192.168.1.108:5173",
+  "http://localhost:6006",
+  "http://192.168.1.108:6006",
 ];
 
 const privateNetworkPattern =
