23cc0dcf6db502836b34ce00fa0bc493ed22d2b1
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-12-18 20:32:25 +0200

    Add gtm to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="gtm-base" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NJ654G92');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJ654G92"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {process.env.NEXT_PUBLIC_GA_ID ? (
           <>
             <Script
